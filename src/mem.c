0e14995a0c112813537bdc2fc62819ea17a855e3
Felix Dangers <fdangers@proton.me>
2026-02-12 20:15:56 +0100

    Implement brkman_mem_alloc() for memory allocation with heap extension support

--- src/mem.c
+++ src/mem.c
@@ -1,4 +1,5 @@
 #include "mem.h"
+#include "attributes.h"
 #include "heap.h"
 
 bool heap_extend()
@@ -14,4 +15,42 @@ bool heap_extend()
     brkman_inc_top_chunk(BRKMAN_HEAP_EXT_SIZE);
 
     return true;
+}
+
+brkman_chunk_t* brkman_mem_alloc(size_t membytes)
+{
+    brkman_chunk_t* ret_chunk = NULL;
+
+    if (membytes < BRKMAN_MIN_CHUNK_SIZE)
+    {
+        membytes = BRKMAN_MIN_CHUNK_SIZE;
+    }
+    else if (!BRKMAN_IS_ALIGNED(membytes))
+    {
+        membytes = BRKMAN_ALIGN_UP(membytes);
+    }
+    else
+    {
+        /* this isn"t considered an error. We simply return a freeable
+         * NULL-pointer, as the caller asked for no memory. */
+        return NULL;
+    }
+
+    size_t free_bytes = brkman_get_free_bytes();
+
+    if (free_bytes < membytes)
+    {
+        /* No sufficient memory left - enlarge heap */
+        if (!heap_extend())
+        {
+            return NULL;
+        }
+    }
+    else
+    {
+        /* sufficient memory left - ask for an appropriate chunk */
+        ret_chunk = brkman_search_free(membytes);
+    }
+
+    return ret_chunk;
 }
