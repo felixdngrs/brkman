cc40e8042871c91b6d070c97442b7fff5b98132d
Felix Dangers <fdangers@proton.me>
2026-02-28 16:55:44 +0100

    remove binary test_prog and enhance test suite with error reporting
    - Remove binary file test/test_prog, as it is generated and should not be versioned.
    - Refactor test functions to return error reasons via const char** parameter:
      - Replace bool return type with bool (*func)(const char** reterr_msg).
      - Add test_errmsgs array for standardized error messages.
      - Use RETERR_MSG macro to set error reasons on failure.
    - Add new test cases:
      - test_alloc_04: Check edge-case allocation near SIZE_MAX.
      - Update test_alloc_03 to explicitly validate overflow behavior.
    - Enhance test_free_01 to verify heap memory is fully freed by comparing heap_ref->free_mem_bytes.
    - Implement test_free_02 to safely handle NULL input.
    - Refactor test runner to print error reasons on failure and reset heap after each test case.
    - Update test_result_t struct to include reason field for storing error details.
    - Include heap.h for chunk and metadata analysis.

--- test/test_runner.c
+++ test/test_runner.c
@@ -1,39 +1,128 @@
 #include "test_runner.h"
 #include "brkman.h"
-#include "mem.h" /* for heap reset */
+#include "heap.h" /* for analyzing chunk and heap metadata*/
+#include "mem.h"  /* for heap reset */
 #include <stdio.h>
 
-bool test_alloc_01(void)
+const char* test_errmsgs[TEST_ERR_MSG_MAX] = {
+    [TEST_ERR_ALLOC_RET_NULL] = "ALLOC_RET_NULL",
+    [TEST_ERR_ALLOC_TOOSMALL] = "ALLOC_TOOSMALL",
+    [TEST_ERR_ALLOC_NULLEXPCTD] = "ALLOC_NULLEXPCTD",
+    [TEST_ERR_FREE_HEAP_NOTCOMPFREED] = "FREE_HEAP_NOTCOMPFREED",
+};
+
+bool test_alloc_01(const char** reterr_msg)
 {
-    void* ptr = brkman_alloc(32);
-    return (ptr != NULL);
+    const size_t mbytes = 32;
+    const void* ptr = brkman_alloc(mbytes);
+
+    if (NULL == ptr)
+    {
+        RETERR_MSG(TEST_ERR_ALLOC_RET_NULL);
+        return false;
+    }
+
+    const brkman_chunk_t* header = brkman_heap_header_of(ptr);
+
+    const bool memory_allocated = header->size >= mbytes;
+
+    return memory_allocated;
 }
 
-bool test_alloc_02(void)
+bool test_alloc_02(const char** reterr_msg)
 {
-    void* ptr = brkman_alloc(1024);
-    return ptr != NULL;
+    const size_t mbytes = 1024;
+    const void* ptr = brkman_alloc(mbytes);
+
+    if (NULL == ptr)
+    {
+        RETERR_MSG(TEST_ERR_ALLOC_RET_NULL);
+        return false;
+    }
+
+    const brkman_chunk_t* header = brkman_heap_header_of(ptr);
+
+    const bool memory_allocated = header->size >= mbytes;
+
+    return memory_allocated;
 }
 
-bool test_alloc_03(void)
+bool test_alloc_03(const char** reterr_msg)
 {
-    void* ptr = brkman_alloc(SIZE_MAX);
-    return (ptr != NULL);
-}
+    const size_t mbytes = SIZE_MAX;
+    const void* ptr = brkman_alloc(mbytes);
 
-bool test_free_01(void)
-{
+    if (NULL == ptr)
+    {
+        return true;
+    }
+
+    RETERR_MSG(TEST_ERR_ALLOC_NULLEXPCTD);
     return false;
 }
 
-bool test_free_02(void)
+bool test_alloc_04(const char** reterr_msg)
 {
+    /* even if we multiply the header size by two here it's still big enough */
+    const size_t mbytes = SIZE_MAX - (BRKMAN_CHUNK_HEADER_SIZE * 2);
+    const void* ptr = brkman_alloc(mbytes);
+
+    if (NULL == ptr)
+    {
+        return true;
+    }
+
+    RETERR_MSG(TEST_ERR_ALLOC_NULLEXPCTD);
     return false;
 }
 
-bool test_free_03(void)
+bool test_free_01(const char** reterr_msg)
 {
-    return false;
+    const size_t mbytes = 1024;
+    const brkman_heap_t* heap_ref = brkman_heap_ref();
+
+    void* ptr = brkman_alloc(mbytes);
+    if (NULL == ptr)
+    {
+        RETERR_MSG(TEST_ERR_ALLOC_RET_NULL);
+        return false;
+    }
+
+    /* get the size of our chunk */
+    const brkman_chunk_t* header = brkman_heap_header_of(ptr);
+    const size_t chunksize = header->size;
+
+    const size_t heap_size_after_alloc = heap_ref->free_mem_bytes;
+
+    brkman_free(ptr);
+
+    const size_t heap_size_after_free = heap_ref->free_mem_bytes;
+
+    /* check if any memory was allocated */
+    const size_t tmp_header_size = BRKMAN_CHUNK_HEADER_SIZE;
+    const bool memory_was_allocated = chunksize >= (mbytes + tmp_header_size);
+    /* check if the memory was fully freed */
+    const size_t expsiz_after_free = heap_size_after_alloc + chunksize;
+    const bool memory_was_freed = heap_size_after_free == expsiz_after_free;
+
+    if (!memory_was_allocated)
+    {
+        RETERR_MSG(TEST_ERR_ALLOC_TOOSMALL);
+        return false;
+    }
+    if (!memory_was_freed)
+    {
+        RETERR_MSG(TEST_ERR_FREE_HEAP_NOTCOMPFREED);
+        return false;
+    }
+    return true;
+}
+
+bool test_free_02(const char** reterr_msg)
+{
+    UNUSED(reterr_msg);
+    brkman_free(NULL);
+    return true;
 }
 
 test_case_t test_list[_TEST_MAX] = {
@@ -46,15 +135,16 @@ test_case_t test_list[_TEST_MAX] = {
      "memory",
      test_alloc_02},
     {"TEST_ALLOC_03",
-     "Here we try to allocate an enormous amount of memory to push the limits "
-     "of the allocator",
+     "Here we try to allocate an enormous amount of memory to check the "
+     "overflow behaviour.",
      test_alloc_03},
+    {"TEST_ALLOC_04",
+     "Here we try to allocate an enormous amount of memory to "
+     "push the heap limits.",
+     test_alloc_04},
     {"TEST_FREE_01", "We allocate some memory and try to free it immediately",
      test_free_01},
     {"TEST_FREE_02", "We try to call free with a NULL pointer", test_free_02},
-    {"TEST_FREE_03",
-     "We try to call free with a pointer that doesn't belong to the heap",
-     test_free_03},
 };
 
 test_result_t test_results[_TEST_MAX];
@@ -77,11 +167,13 @@ bool test_runner()
     {
         test_case_t* current_test_case = &(test_list[test_id]);
         test_result_t* current_test_result = &(test_results[test_id]);
-
+        const char** current_test_result_errreason =
+            &current_test_result->reason;
         /* save ref to current test case in test result */
         current_test_result->test_case = current_test_case;
 
-        bool test_success = (current_test_case->func)();
+        bool test_success =
+            (current_test_case->func)(current_test_result_errreason);
         if (test_success)
         {
             current_test_result->result = TEST_RESULT_SUCCESS;
@@ -90,6 +182,15 @@ bool test_runner()
         {
             success = false;
             current_test_result->result = TEST_RESULT_FAILURE;
+            fprintf(stderr, "current_test_result->reason: %s\n",
+                    current_test_result->reason);
+        }
+        bool heap_reset_status = brkman_heap_reset();
+        if (!heap_reset_status)
+        {
+            fprintf(
+                stderr,
+                "\033[35m\t --> failed to reset heap after test case\033[0m\n");
         }
     }
     return success;
@@ -101,6 +202,8 @@ void print_test_results(void)
     {
         test_result_t* current_test_result = &(test_results[test_id]);
         test_case_t* current_test_case = current_test_result->test_case;
+        const char* current_test_result_errreason = current_test_result->reason;
+
         if (current_test_result->result == TEST_RESULT_UNKNOWN)
         {
             fprintf(stdout, "\033[34m[NOT RUN]\033[0m\t");
@@ -115,12 +218,10 @@ void print_test_results(void)
         }
         fprintf(stderr, "%16s - \"%s\"\n", current_test_case->name,
                 current_test_case->descr);
-        bool heap_reset_status = brkman_heap_reset();
-        if (!heap_reset_status)
+        if (current_test_result->result == TEST_RESULT_FAILURE)
         {
-            fprintf(
-                stderr,
-                "\033[35m\t --> failed to reset heap after test case\033[0m\n");
+            fprintf(stderr, "\treason: \"%s\"\n",
+                    current_test_result_errreason);
         }
     }
 }
--- test/test_runner.h
+++ test/test_runner.h
@@ -1,5 +1,22 @@
 #include <stdbool.h>
 
+#define RETERR_MSG(id)                                                         \
+    do                                                                         \
+    {                                                                          \
+        *reterr_msg = test_errmsgs[id];                                        \
+    } while (0)
+
+typedef enum test_errmsg_e
+{
+    TEST_ERR_ALLOC_RET_NULL,
+    TEST_ERR_ALLOC_TOOSMALL,
+    TEST_ERR_ALLOC_NULLEXPCTD,
+    TEST_ERR_FREE_HEAP_NOTCOMPFREED,
+    TEST_ERR_MSG_MAX
+} test_errmsg_e;
+
+extern const char* test_errmsgs[TEST_ERR_MSG_MAX];
+
 /**
  * @enum test_id_e
  * @brief Enumeration of all available test case identifiers.
@@ -12,9 +29,9 @@ typedef enum test_id_e
     TEST_ALLOC_01,
     TEST_ALLOC_02,
     TEST_ALLOC_03,
+    TEST_ALLOC_04,
     TEST_FREE_01,
     TEST_FREE_02,
-    TEST_FREE_03,
     _TEST_MAX
 } test_id_e;
 
@@ -40,9 +57,10 @@ typedef enum test_result_e
  */
 typedef struct test_case_t
 {
-    const char* name;   /**< Short name/identifier of the test case. */
-    const char* descr;  /**< Detailed description of the test case. */
-    bool (*func)(void); /**< Function pointer to the test implementation. */
+    const char* name;  /**< Short name/identifier of the test case. */
+    const char* descr; /**< Detailed description of the test case. */
+    bool (*func)(
+        const char**); /**< Function pointer to the test implementation. */
 } test_case_t;
 
 /**
@@ -55,6 +73,7 @@ typedef struct test_result_t
 {
     test_case_t* test_case; /**< Pointer to the executed test case. */
     test_result_e result;   /**< Result of the test execution. */
+    const char* reason;     /**< Error reason (if an error occured). */
 } test_result_t;
 
 /**
